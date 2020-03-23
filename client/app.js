ec23bf829122cb3f22087b7db7891ffe33ad73ab
Pawel Bozemski <paw.boz@gmail.com>
2020-03-23 14:19:01 +0100

    Add send messages

--- client/app.js
+++ client/app.js
@@ -7,3 +7,53 @@ const messageContentInput = document.getElementById('message-content');
 
 let userName = '';
 
+// funtctions
+
+function login (e) {
+  e.preventDefault();
+
+  if(userNameInput.value !== '') {
+    userName = userNameInput.value;
+    loginForm.classList.toggle('show');
+    messagesSection.classList.add('show');
+  } else {
+    alert('Please enter your login');
+  };
+};
+
+function sendMessage(e) {
+  e.preventDefault();
+
+  if(messageContentInput.value !== '') {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput = '';
+  } else {
+    alert ('Type your message');
+  };
+}
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
+
+// listeners
+
+loginForm.addEventListener('submit', e => {
+  login(e);
+});
+
+addMessageForm.addEventListener('submit', e => {
+  sendMessage(e);
+})
+
+
