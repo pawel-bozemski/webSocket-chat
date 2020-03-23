3643c0d68448bfa9b1108ad91028543cc1009e08
Pawel Bozemski <paw.boz@gmail.com>
2020-03-23 13:23:36 +0100

    Remove show class and add global variable

--- client/app.js
+++ client/app.js
@@ -4,3 +4,6 @@ const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
+
+let userName = '';
+
