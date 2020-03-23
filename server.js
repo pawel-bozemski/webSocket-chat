9d785b6ddc7f0d26bc2decb44e46cc4274cfd7ca
Pawel Bozemski <paw.boz@gmail.com>
2020-03-23 15:05:50 +0100

    Add showing msgs for users

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
@@ -11,6 +12,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
-app.listen(8000, () => {
-  console.log('Server running on port 8000');
+const server = app.listen(8000, () => {
+  console.log('Server is running on Port:', 8000)
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left') });
+  console.log('I\'ve added a listener on message and disconnect events \n');
 });
