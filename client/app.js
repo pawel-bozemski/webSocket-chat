2ea8edddb90d4074a3420d8c14071a748817eac9
Pawel Bozemski <paw.boz@gmail.com>
2020-03-23 13:18:04 +0100

    init project

---
+++ client/app.js
@@ -0,0 +1,6 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
